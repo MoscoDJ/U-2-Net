f0557f31007bfc76078613b9b6e003eee4e82a68
MoscoDJ <MoscoDJ@users.noreply.github.com>
2024-08-06 22:52:07 -0600

    Create setup.py
    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='u2net',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchvision',
+        'Pillow'
+    ],
+)
